f26d7ac48173e2fbea1e3499766cf6c15362b6b8
sibaluca12 <sibaluca@gmail.com>
2020-02-04 14:40:41 +0100

    paddle moving

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,39 @@
-something
+
+
+window.onload = function() {
+
+
+    let paddle = {
+        left: 500,
+        top: 500
+    };
+
+    document.onkeydown = function(e) {
+            if (e.keyCode == 37) {
+                console.log('something')
+                paddle.left = paddle.left - 20;
+
+            }
+            if (e.keyCode == 39) {
+                paddle.left = paddle.left + 20;
+                console.log('something else')
+            }
+            drawPaddle();
+        }
+
+
+    function drawPaddle() {
+        document.getElementById('paddle').style.left = paddle.left + 'px';
+        document.getElementById('paddle').style.top = paddle.top + 'px';
+
+    }
+
+
+
+    function draw() {
+        drawPaddle()
+    }
+
+    draw()
+}
+
