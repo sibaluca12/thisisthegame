71cb4413d90c114b2892c3e38fb26a3c615bf988
pivarcsi <pivarcsiadam@gmail.com>
2020-02-04 17:37:37 +0100

    level background

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,17 @@
-something
+function level_add(new_level_id, old_level_id) {
+    let level1 = document.createElement("div");
+    level1.setAttribute('id', new_level_id);
+    let old_page = document.getElementById(old_level_id);
+    old_page.remove();
+    let main_page = document.getElementById('background');
+    main_page.appendChild(level1)
+}
+
+function main() {
+    let main_page_id = 'playground';
+    let level1_id = 'playground_level1';
+    document.getElementById('start').addEventListener("click", function(){
+        level_add(level1_id, main_page_id)});
+    }
+
+main();
