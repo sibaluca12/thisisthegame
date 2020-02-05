0f6a75f555862a43b0c01e195a80ac8f197739ed
pivarcsi <pivarcsiadam@gmail.com>
2020-02-05 18:26:02 +0100

    merge complete

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,108 @@
 window.onload = function () {
+    const speak_card = {
+        level1: ['/static/images/speaks/level1-1.png', '/static/images/speaks/level1-2.png', '/static/images/speaks/level1-3.png', '/static/images/speaks/level1-4.png', '/static/images/speaks/level1-5.png'],
+        level2: ['/static/images/speaks/level2-1.png', '/static/images/speaks/level2-2.png', '/static/images/speaks/level2-3.png', '/static/images/speaks/level2-4.png', '/static/images/speaks/level2-5.png'],
+        level3: ['/static/images/speaks/level3-1.png', '/static/images/speaks/level3-2.png', '/static/images/speaks/level3-3.png', '/static/images/speaks/level3-4.png', '/static/images/speaks/level3-5.png'],
+        level4: ['/static/images/speaks/level4-1.png', '/static/images/speaks/level4-2.png', '/static/images/speaks/level4-3.png', '/static/images/speaks/level4-4.png', '/static/images/speaks/level4-5.png']
+
+    };
+    const game_data = {life:0, score:0, level:'level1'};
     document.getElementById('paddle').style.display = 'none';
-    document.getElementById('start').addEventListener('click', play)
+    document.getElementById('start').addEventListener('click', function(){difficult(game_data, speak_card)})
 };
 
+function difficult(game_data, speak_card){
+    document.getElementById('start').remove();
+    document.getElementById('tutorial').remove();
+    const difficult_buttons = document.createElement('div');
+    difficult_buttons.setAttribute('id', 'difficult_button');
+
+    let difficult = ['Easy', 'Medium', 'Mandalorian'];
+    for (let element of difficult){
+        let button = document.createElement('button');
+        button.textContent = element;
+        button.setAttribute('id', element);
+        difficult_buttons.appendChild(button)}
+    const background = document.getElementById('menu_button');
+    background.appendChild(difficult_buttons);
+
+    for (let element of difficult){
+        document.getElementById(element).addEventListener('click', function(){
+            game_start(element, game_data, speak_card)})
+    }
+}
+
+function game_start(difficult, game_data, speak_card){
+    set_life(difficult, game_data);
+    new_level('/static/images/level1_background.jpg');
+    add_life_bar(game_data.life);
+    add_score_bar(game_data.score);
+    speak(speak_card)
+
+}
+
+function set_life(difficult, game_data){
+    if (difficult === 'Easy'){
+        game_data.life = 5
+    }
+    else if (difficult === 'Medium'){
+        game_data.life = 3
+    }
+    else {
+        game_data.life = 1
+    }
+}
+
+function new_level(new_background){
+    document.getElementById('menu_button').remove();
+    document.getElementById('playground').style.backgroundImage = `url('${new_background}')`;
+}
+
+function add_life_bar(live){
+    const life_bar = document.createElement('div');
+    life_bar.setAttribute('id', 'life_bar');
+
+    for (let i=0; i<live; i++){
+        let life = document.createElement('img');
+        life.setAttribute('src', '/static/images/life_bar.png');
+        life.setAttribute('id', 'life');
+        life_bar.appendChild(life);
+    }
+
+    document.getElementById('background').appendChild(life_bar)
+}
+
+function add_score_bar(score) {
+    const score_bar = document.createElement('div');
+    score_bar.setAttribute('id', 'score_bar');
+    score_bar.textContent = score;
+
+    document.getElementById('background').appendChild(score_bar)
+}
+
+function speak_change(speak_card,){
+    let old_image = document.getElementById('speak').getAttribute('src');
+    let index = speak_card.level1.indexOf(old_image);
+    index++;
+    if (index < speak_card.level1.length){
+        document.getElementById('speak').setAttribute('src', speak_card.level1[index])
+    }
+    else if (index === speak_card.level1.length){
+        document.getElementById('speak').addEventListener('click', play)
+    }
+}
+
+function speak(speak_card){
+    let speak = document.createElement('img');
+    speak.setAttribute('id', 'speak');
+    speak.setAttribute('src', speak_card.level1[0]);
+    document.getElementById('playground').appendChild(speak);
+
+    document.getElementById('speak').addEventListener('click', function(){speak_change(speak_card)});
+}
 
 function play() {
+    document.getElementById('speak').style.display = 'none';
     document.getElementById('paddle').style.display = 'block';
     let playGround = document.getElementById('playground');
     let paddleWidth = 150;
