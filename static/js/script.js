cd05a31f8054a45a5d90db1cbdd7614253c5b971
pivarcsi <pivarcsiadam@gmail.com>
2020-02-06 17:19:15 +0100

    death and back the menu

--- static/js/script.js
+++ static/js/script.js
@@ -105,6 +105,7 @@ function new_level(new_background) {
 }
 
 function add_life_bar(live) {
+    document.getElementById('life_bar').remove();
     const life_bar = document.createElement('div');
     life_bar.setAttribute('id', 'life_bar');
 
@@ -119,11 +120,7 @@ function add_life_bar(live) {
 }
 
 function add_score_bar(score) {
-    const score_bar = document.createElement('div');
-    score_bar.setAttribute('id', 'score_bar');
-    score_bar.textContent = score;
-
-    document.getElementById('background').appendChild(score_bar)
+    document.getElementById('score_bar').textContent = score;
 }
 
 function speak_change(speak_card, game_data) {
@@ -152,254 +149,251 @@ function speak(speak_card, game_data) {
 }
 
 function play(game_data) {
-    document.getElementById('speak').style.display = 'none';
-    document.getElementById('paddle').style.display = 'block';
-    let playGround = document.getElementById('playground');
-    let paddleWidth = 130;
-    let paddleHeight = 10;
-    let brickWidth = 10;
-    let brickHeight = 10;
-    let leftPressed = false;
-    let playGroundWidth = document.getElementById('playground').clientWidth;
-    let playGroundHeight = document.getElementById('playground').clientHeight;
-    let heads = ['Gamorrean_head.png', 'Gran_head.png', 'Weequay_head.png'];
+    if (game_data.move) {
+        document.getElementById('speak').style.display = 'none';
+        document.getElementById('paddle').style.display = 'block';
+        let paddleWidth = 130;
+        let brickWidth = 10;
+        let brickHeight = 10;
+        let leftPressed = false;
+        let playGroundWidth = document.getElementById('playground').clientWidth;
+        let playGroundHeight = document.getElementById('playground').clientHeight;
+        // let heads = ['Gamorrean_head.png', 'Gran_head.png', 'Weequay_head.png'];
 
-    let paddle = {
-        left: 40,
-        top: 93.2
-    };
+        let paddle = {
+            left: 40,
+            top: 93.2
+        };
 
-    let bricks = [
-        { left: 80, top: 0, status:1},
-        { left: 85, top: 15,status:1},
-        { left: 90, top: 30, status:1 },
-        { left: 60, top: 0, status:1 },
-        { left: 65, top: 15,status:1 },
-        { left: 70, top: 30,status:1 },
-        { left: 40, top: 0, status:1 },
-        { left: 45, top: 15, status:1 },
-        { left: 50, top: 30, status:1 },
-        { left: 20, top: 0, status:1},
-        { left: 25, top: 15, status:1 },
-        { left: 30, top: 30, status:1 },
-        { left: 0, top: 0, status:1 },
-        { left: 5, top: 15, status:1 },
-        { left: 10, top: 30, status:1 },
-    ];
+        let bricks = [
+            {left: 80, top: 0, status: 1},
+            {left: 85, top: 15, status: 1},
+            {left: 90, top: 30, status: 1},
+            {left: 60, top: 0, status: 1},
+            {left: 65, top: 15, status: 1},
+            {left: 70, top: 30, status: 1},
+            {left: 40, top: 0, status: 1},
+            {left: 45, top: 15, status: 1},
+            {left: 50, top: 30, status: 1},
+            {left: 20, top: 0, status: 1},
+            {left: 25, top: 15, status: 1},
+            {left: 30, top: 30, status: 1},
+            {left: 0, top: 0, status: 1},
+            {left: 5, top: 15, status: 1},
+            {left: 10, top: 30, status: 1},
+        ];
 
-    document.onmousemove = function (event) {
-        if (event.clientX >= paddleWidth / 2 && event.clientX <= (playGroundWidth - paddleWidth / 2)) {
-            paddle.left = event.clientX - paddleWidth / 2;
-        } else if (paddle.left < paddleWidth / 2) {
-            paddle.left = 0;
+        if (game_data.move) {
+            document.onmousemove = function (event) {
+                if (event.clientX >= paddleWidth / 2 && event.clientX <= (playGroundWidth - paddleWidth / 2)) {
+                    paddle.left = event.clientX - paddleWidth / 2;
+                } else if (paddle.left < paddleWidth / 2) {
+                    paddle.left = 0;
+                } else if (paddle.left > playGroundWidth - paddleWidth / 2 || event.clientX > playGroundWidth) {
+                    paddle.left = playGroundWidth - paddleWidth;
+                }
+               drawPaddle()
+            };
         }
-        else if (paddle.left > playGroundWidth - paddleWidth / 2 || event.clientX > playGroundWidth){
-            paddle.left = playGroundWidth - paddleWidth;
-        }
-        drawPaddle()
-    };
 
-    document.onkeydown = function (e) {
-        let rightPressed;
-        if (e.code === 'ArrowLeft') {
-            if (paddle.left > 0) {
-                paddle.left = paddle.left - 2;
+        document.onkeydown = function (e) {
+            if (e.code === 'ArrowLeft') {
+                if (paddle.left > 0) {
+                    paddle.left = paddle.left - 5;
+                }
+            }
+            if (e.code === 'ArrowRight') {
+                if (paddle.left < 79.5) {
+                    leftPressed = true;
+                    paddle.left = paddle.left + 5;
+                }
+            }
+
+            drawPaddle();
+        };
+
+        function coordinate(element) {
+            return element.getBoundingClientRect();
+        }
+
+        // function getRandom(min, max) {
+        //     return Math.floor(Math.random() * (max - min) + min)
+        // }
+
+        function drawBricks() {
+            document.getElementById('bricks').innerHTML = "";
+            for (let i = 0; i < bricks.length; i++) {
+                document.getElementById('bricks').innerHTML += `<div class='bricks' style='left:${bricks[i].left}%; top:${bricks[i].top}%' id="brick${i}"></div>`;
             }
         }
-        if (e.code === 'ArrowRight') {
-            if (paddle.left < 79.5) {
-                leftPressed = true;
-                paddle.left = paddle.left + 2;
+
+        // function add_head() {
+        //     let index = getRandom(0, heads.length);
+        //     return `/static/images/${heads[index]}`;
+        // }
+
+        function drawPaddle() {
+            document.getElementById('paddle').style.left = paddle.left + 'px';
+            document.getElementById('paddle').style.top = paddle.top + '%';
+        }
+
+        let ball = document.createElement('div');
+        ball.id = 'ball';
+        document.querySelector('#playground').appendChild(ball);
+
+        let ballX = 60;
+        let ballY = 20;
+        let ballRadius = 10;
+        let ballRadiusHorizontalPercentage = (ballRadius / playGroundWidth) * 100;
+        let ballRadiusVerticalPercentage = (ballRadius / playGroundHeight) * 100;
+
+        Object.assign(ball.style, {
+            width: ballRadius * 2 + 'px',
+            height: ballRadius * 2 + 'px',
+            borderRadius: '50%',
+            backgroundColor: 'orangered',
+            position: 'absolute',
+            left: ballX + '%',
+            bottom: ballY + '%',
+        });
+
+        let direction = {dx: game_data.speed, dy: game_data.speed};
+
+        if (game_data.move) {
+            setInterval(move, 20);
+        }
+
+        function collisionDetection() {
+            for (let brick = 0; brick < bricks.length; brick++) {
+                let currentBrickLeft = bricks[brick].left;
+                let currentBrickTop = bricks[brick].top;
+                let rightOfTheBallX = ballX + 2 * ballRadiusHorizontalPercentage;
+                let topOfTheBallY = ballY + 2 * ballRadiusVerticalPercentage;
+                let topOfCurrentBrickY = 100 - currentBrickTop;
+                let bottomOfCurrentBrickY = topOfCurrentBrickY - brickHeight;
+                if (
+                    rightOfTheBallX >= currentBrickLeft &&
+                    ballX <= currentBrickLeft + brickWidth &&
+                    ballY <= topOfCurrentBrickY &&
+                    topOfTheBallY >= bottomOfCurrentBrickY
+                ) {
+                    if (ballY < topOfCurrentBrickY - 2 && topOfTheBallY > bottomOfCurrentBrickY + 2) {
+                        direction.dx *= -1;
+                    } else {
+                        direction.dy *= -1;
+                    }
+                    bricks.splice(brick, 1);
+                    game_data.score += game_data.point;
+                    document.getElementById('score_bar').textContent = game_data.score
+                }
             }
+            drawBricks();
         }
 
-        drawPaddle();
-    };
-
-    function coordinate(element) {
-        return element.getBoundingClientRect();
-    }
-
-    function getRandom(min, max) {
-        return Math.floor(Math.random() * (max - min) + min)
-    }
-
-    function drawBricks() {
-        document.getElementById('bricks').innerHTML = "";
-        for (let i = 0; i < bricks.length; i++) {
-            document.getElementById('bricks').innerHTML += `<div class='bricks' style='left:${bricks[i].left}%; top:${bricks[i].top}%' id="brick${i}"></div>`;
+        function drawBall() {
+            Object.assign(ball.style, {
+                left: ballX + '%',
+                bottom: ballY + '%'
+            });
         }
-    }
 
-    function add_head(){
-        let index = getRandom(0, heads.length);
-        return `/static/images/${heads[index]}`;
-    }
 
-    function drawPaddle() {
-        document.getElementById('paddle').style.left = paddle.left + 'px';
-        document.getElementById('paddle').style.top = paddle.top + '%';
-    }
-
-    let ball = document.createElement('div');
-    ball.id = 'ball';
-    document.querySelector('#playground').appendChild(ball);
-
-    let ballX = 60;
-    let ballY = 20;
-    let ballRadius = 10;
-    let ballRadiusHorizontalPercentage = (ballRadius / playGroundWidth) * 100;
-    let ballRadiusVerticalPercentage = (ballRadius / playGroundHeight) * 100;
-
-    Object.assign(ball.style, {
-        width: ballRadius * 2 + 'px',
-        height: ballRadius * 2 + 'px',
-        borderRadius: '50%',
-        backgroundColor: 'orangered',
-        position: 'absolute',
-        left: ballX + '%',
-        bottom: ballY + '%',
-    });
-
-    let direction = {dx: game_data.speed, dy: game_data.speed};
-
-    let id = setInterval(move, 20);
-
-    function collisionDetection() {
-        for (let brick = 0; brick < bricks.length; brick++) {
-            let currentBrickLeft = bricks[brick].left;
-            let currentBrickTop = bricks[brick].top;
-            let rightOfTheBallX = ballX + 2 * ballRadiusHorizontalPercentage;
-            let topOfTheBallY = ballY + 2 * ballRadiusVerticalPercentage;
-            let topOfCurrentBrickY = 100 - currentBrickTop;
-            let bottomOfCurrentBrickY = topOfCurrentBrickY - brickHeight;
-            if (
-                rightOfTheBallX >= currentBrickLeft &&
-                ballX <= currentBrickLeft + brickWidth &&
-                ballY <= topOfCurrentBrickY &&
-                topOfTheBallY >= bottomOfCurrentBrickY
+        function paddleCollision() {
+            let paddleWidthPercentage = (paddleWidth / playGroundWidth) * 100;
+            let bottomOfPaddleY = 100 - paddle.top;
+            let leftOfPaddleX = (paddle.left / playGroundWidth) * 100;
+            if (ballX + ballRadiusHorizontalPercentage >= leftOfPaddleX &&
+                ballX <= leftOfPaddleX + paddleWidthPercentage &&
+                ballY <= bottomOfPaddleY
             ) {
-                if (ballY < topOfCurrentBrickY - 2 && topOfTheBallY > bottomOfCurrentBrickY + 2) {
+                direction.dy *= -1;
+
+            }
+
+        }
+
+        function reset_board() {
+            document.getElementById('score_bar').textContent = '0';
+            document.getElementById('playground').remove();
+            document.getElementById('song').setAttribute('src', '/static/audio/The_Mandalorian%20-%20Soundtrack.mp3');
+        }
+
+        function back_to_menu() {
+            reset_board();
+            let play_ground = document.createElement('div');
+            play_ground.id = 'playground';
+
+            let paddle = document.createElement('div');
+            paddle.id = 'paddle';
+            paddle.classList.add('paddle');
+            play_ground.appendChild(paddle);
+
+            let bricks = document.createElement('div');
+            bricks.id = 'bricks';
+            play_ground.appendChild(bricks);
+
+            let menu_button = document.createElement('div');
+            menu_button.id = 'menu_button';
+
+            let start_button = document.createElement('button');
+            start_button.id = 'start';
+            start_button.textContent = 'START';
+
+            menu_button.appendChild(start_button);
+            play_ground.appendChild(menu_button);
+            document.getElementById('background').appendChild(play_ground);
+            document.getElementById('playground').style.backgroundImage = '/static/images/menu_background.jpg';
+            menu()
+        }
+
+        function death() {
+            let death = document.createElement('div');
+            death.id = 'death';
+            death.textContent = 'MISSION FAIL';
+            let ballCoord = coordinate(document.getElementById('ball'));
+            let paddleCoord = coordinate(document.getElementById('paddle'));
+            if (ballCoord.y >= paddleCoord.y + 10) {
+                game_data.life -= 1;
+                add_life_bar(game_data.life);
+                ballX = 60;
+                ballY = 20;
+                direction.dy *= -1;
+                if (game_data.life === 0) {
+                    game_data.move = false;
+                    document.getElementById('playground').appendChild(death);
+                    setTimeout(back_to_menu, 3000)
+                }
+            }
+        }
+
+        function move() {
+            if (game_data.move) {
+                if (ballX >= 100 - 2 * ballRadiusHorizontalPercentage || ballX <= 0) {
                     direction.dx *= -1;
-                } else {
+                }
+                if (ballY >= 100 - 2 * ballRadiusVerticalPercentage || ballY <= 0) {
                     direction.dy *= -1;
                 }
-                bricks.splice(brick, 1);
-                game_data.score += game_data.point;
-                document.getElementById('score_bar').textContent = game_data.score
+                collisionDetection();
+                death();
+                paddleCollision();
+
+                if (ballY >= 110 || ballY <= -10 || ballX <= -10 || ballX >= 110) {
+                    clearInterval();
+                } else {
+                    ballX += direction.dx;
+                    ballY += direction.dy;
+                    drawBall();
+                }
             }
         }
-        drawBricks();
-    }
 
-    function drawBall() {
-        Object.assign(ball.style, {
-            left: ballX + '%',
-            bottom: ballY + '%'
-        });
-    }
-
-
-    function paddleCollision() {
-        let paddleWidthPercentage = (paddleWidth / playGroundWidth) * 100;
-        let bottomOfPaddleY = 100 - paddle.top;
-        let leftOfPaddleX = (paddle.left / playGroundWidth) * 100;
-        if(ballX + ballRadiusHorizontalPercentage >= leftOfPaddleX  &&
-            ballX <= leftOfPaddleX + paddleWidthPercentage  &&
-            ballY <= bottomOfPaddleY
-    ) {
-           direction.dy *= -1;
-
+        function draw() {
+            drawPaddle();
+            drawBricks();
+            move();
         }
 
+        draw();
     }
-
-    function reset_board(){
-        document.getElementById('life').remove();
-        document.getElementById('life_bar').remove();
-        document.getElementById('score_bar').remove();
-        document.getElementById('playground').remove();
-        document.getElementById('song').setAttribute('src', '/static/audio/The_Mandalorian%20-%20Soundtrack.mp3');
-        for (let i = 0; i < bricks.length; i++){
-            document.getElementById(`brick${i}`).remove()
-        }
-    }
-
-    function back_to_menu(){
-        reset_board();
-        let play_ground = document.createElement('div');
-        play_ground.id = 'playground';
-
-        let paddle = document.createElement('div');
-        paddle.id = 'paddle';
-        paddle.classList.add('paddle');
-        play_ground.appendChild(paddle);
-
-        let bricks = document.createElement('div');
-        bricks.id = 'bricks';
-        play_ground.appendChild(bricks);
-
-        let menu_button = document.createElement('div');
-        menu_button.id = 'menu_button';
-
-        let start_button = document.createElement('button');
-        start_button.id = 'start';
-        start_button.value = 'START';
-
-        menu_button.appendChild(start_button);
-        play_ground.appendChild(menu_button);
-        document.getElementById('background').appendChild(play_ground);
-        document.getElementById('playground').style.backgroundImage = '/static/images/menu_background.jpg';
-        menu()
-    }
-
-    function death(){
-        let death = document.createElement('div');
-        death.id = 'death';
-        death.textContent = 'MISSION FAIL';
-        let ballCoord = coordinate(document.getElementById('ball'));
-        let paddleCoord = coordinate(document.getElementById('paddle'));
-        if (ballCoord.y >= paddleCoord.y + 10) {
-            game_data.life -= 1;
-            add_life_bar(game_data.life);
-            ballX = 60;
-            ballY = 20;
-            direction.dy *= -1;
-            if (game_data.life === 0) {
-                game_data.move = false;
-                document.getElementById('playground').appendChild(death);
-                setTimeout(back_to_menu, 3000)
-            }
-        }
-    }
-
-    function move() {
-        if (game_data.move) {
-            if (ballX >= 100 - 2 * ballRadiusHorizontalPercentage || ballX <= 0) {
-                direction.dx *= -1;
-            }
-            if (ballY >= 100 - 2 * ballRadiusVerticalPercentage || ballY <= 0) {
-                direction.dy *= -1;
-            }
-            collisionDetection();
-            death();
-            paddleCollision();
-
-            if (ballY >= 110 || ballY <= -10 || ballX <= -10 || ballX >= 110) {
-                clearInterval(id);
-            } else {
-                ballX += direction.dx;
-                ballY += direction.dy;
-                drawBall();
-            }
-        }
-    }
-
-    function draw() {
-        drawPaddle();
-        drawBricks();
-        move();
-    }
-    draw();
-
 }
